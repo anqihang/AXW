e5c2168b7df1ba7b9199405527b2f22f89f1d989
anqihang <1711404616@qq.com>
2025-05-30 20:16:57 +0800

    request

--- miniprogram/api/request.ts
+++ miniprogram/api/request.ts
@@ -34,7 +34,34 @@ function refresh1() {
     },
   });
 }
-export function Request() {}
+type BasicProps = {
+  data?: WechatMiniprogram.RequestOption["data"];
+  header?: WechatMiniprogram.RequestOption["header"];
+};
+export function Request(
+  url: WechatMiniprogram.RequestOption["url"],
+  method: WechatMiniprogram.RequestOption["method"],
+  { data, header }: BasicProps
+) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url,
+      method,
+      data,
+      header,
+      success(res) {
+        if (res.statusCode === 200) {
+          resolve(res.data);
+        } else if (res.statusCode === 403) {
+          if (!isRefresh) {
+            isRefresh = true;
+            refresh1();
+          } else {
+          }
+      }
+    })
+    })
+}
 /**
  * @description 刷新token
  */
