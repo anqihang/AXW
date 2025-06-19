947757b5db1e2f45ab780d5082d61b41270bc9f3
anqihang <1711404616@qq.com>
2025-06-19 20:23:39 +0800

    request

---
+++ typings/api.d.ts
@@ -0,0 +1,11 @@
+export interface Account {
+  id: number;
+  username: string;
+  nickName: string;
+  sex: number;
+  age: number;
+  phone: string;
+  email: string;
+  createTime: string;
+  updateTime: string;
+}
