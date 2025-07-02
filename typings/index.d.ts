fc46bdfdce511fd6f4e61022559da0e8e10e6580
anqihang <1711404616@qq.com>
2025-07-02 20:16:27 +0800

    requestType

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,7 +4,7 @@ interface IAppOption {
   globalData: {
     userInfo: WechatMiniprogram.UserInfo | any; // 微信用户信息
     defaultAvatarUrl: String; // 默认头像
-    accountInfo: WechatMiniprogram.AccountInfo; // 小程序账号信息（线上）
+    accountInfo: WechatMiniprogram.AccountInfo; // 小程序账号信息（正式版线上）
     appBaseInfo: WechatMiniprogram.AppBaseInfo; // 小程序基础信息
     systemInfo: WechatMiniprogram.SystemInfo; // 系统信息
     deviceInfo: WechatMiniprogram.DeviceInfo; // 设备信息
