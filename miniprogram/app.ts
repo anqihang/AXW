0b12da0b7d3648c72d8fd2e33080860ab6d515e3
安琦航 <anqihang0106@outlook.com>
2025-07-05 01:19:24 +0800

    transition

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -22,6 +22,12 @@ App<IAppOption>({
     this.globalData.windowInfo = wx.getWindowInfo();
     console.log(this.globalData.windowInfo, "windowInfo");
 
+    wx.getSetting({
+      withSubscriptions:true,
+      success(res){
+        console.log(res);
+      }
+    })
     // 获取局域网ip地址
     // wx.getLocalIPAddress({
     //   success: (res) => {
