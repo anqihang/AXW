54ba6b7c8d599cd5c01be374d447a3907d055a2b
anqihang <1711404616@qq.com>
2025-07-15 19:59:59 +0800

    pages

---
+++ miniprogram/router/pages.ts
@@ -0,0 +1,37 @@
+const PAGES = {
+  index: {
+    title: "",
+    path: "/pages/index/index",
+    description: "根页面",
+    level: 0,
+  },
+  home: {
+    title: "Home",
+    path: "/pages/index/index?id=1",
+    description: "首页",
+    level: 1,
+  },
+  contact: {
+    title: "Contact",
+    path: "/pages/index/index",
+    description: "Contact page",
+  },
+  mine: {
+    title: "",
+    path: "/pages/index/index?id=5",
+    description: "我的",
+    level: 1,
+  },
+  sign: {
+    title: "",
+    path: "/packageA/sign/sign",
+    description: "注册登录",
+    level: 2,
+  },
+  about: {
+    title: "About",
+    path: "/pages/index/index",
+    description: "About page",
+    level: 2,
+  },
+};
