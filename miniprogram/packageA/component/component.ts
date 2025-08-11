5a1f72d91fafcfcea02cef18995e43615e784c64
安琦航 <anqihang0106@outlook.com>
2025-08-12 01:18:33 +0800

    scroll

--- miniprogram/packageA/component/component.ts
+++ miniprogram/packageA/component/component.ts
@@ -8,6 +8,7 @@ Component({
       { id: 5, name: "toast", path: "/packageComponents/toast/toast" },
       { id: 2, name: "input", path: "/packageComponents/input/input" },
       { id: 3, name: "check", path: "/packageComponents/check/check" },
+      { id: 3, name: "scrol", path: "/packageComponents/scroll/scroll" },
     ],
   },
 
